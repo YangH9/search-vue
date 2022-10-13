fef5d1edf89740e7a9fcc0f2f4cc59d1dfc9e1c9
YangH9 <49155616+YangH9@users.noreply.github.com>
2022-10-13 11:12:29 +0800

    2022/10/13 11:12

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <search />
+  </div>
+</template>
+
+<script>
+import search from "./components/search.vue";
+
+export default {
+  name: "App",
+  components: {
+    search,
+  },
+};
+</script>
+
+<style>
+</style>
